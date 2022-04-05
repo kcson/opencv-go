206883416d28fe10c006e8eee0dcbf6bbfdb9366
kcson <fob330@gmail.com>
2022-04-05 09:08:39 +0900

    commit

---
+++ ch06 /sobel_edge.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"gocv.io/x/gocv"
+	"opencv-go/common"
+	"os"
+)
+
+func main() {
+	src := gocv.IMRead("imgs/lenna.bmp", gocv.IMReadGrayScale)
+	if src.Empty() {
+		fmt.Println("image read fail!!")
+		os.Exit(-1)
+	}
+	defer src.Close()
+
+	srcWin := gocv.NewWindow("src")
+	defer srcWin.Close()
+	srcWin.IMShow(src)
+
+	kernel := common.NewMat(
+		3,
+		3,
+		[][]float32{
+			{-1, 0, 1},
+			{-2, 0, 2},
+			{-1, 0, 1},
+		}, gocv.MatTypeCV32F)
+	defer kernel.Close()
+
+	dx := gocv.NewMat()
+	defer dx.Close()
+	dy := gocv.NewMat()
+	defer dy.Close()
+
+	//gocv.Filter2D(src, &dx, -1, *kernel, image.Pt(-1, -1), 128, gocv.BorderDefault)
+
+	//sobel 함수
+	gocv.Sobel(src, &dx, gocv.MatTypeCV32F, 1, 0, 3, 1, 0, gocv.BorderDefault)
+	gocv.Sobel(src, &dy, gocv.MatTypeCV32F, 0, 1, 3, 1, 0, gocv.BorderDefault)
+
+	mag := gocv.NewMat()
+	defer mag.Close()
+
+	gocv.Magnitude(dx, dy, &mag)
+	mag.ConvertTo(&mag, gocv.MatTypeCV8U)
+	magWin := gocv.NewWindow("mag")
+	defer magWin.Close()
+	magWin.IMShow(mag)
+
+	edge := gocv.Zeros(src.Rows(), src.Cols(),  gocv.MatTypeCV8U)
+	defer edge.Close()
+
+	//gocv.Threshold(mag, &edge, 80, 255, gocv.ThresholdBinary)
+	var threshold uint8 = 120
+	for i := 0; i < mag.Rows(); i++ {
+		for j := 0; j < mag.Cols(); j++ {
+			if mag.GetUCharAt(i, j) > threshold {
+				edge.SetUCharAt(i, j, 255)
+			}
+		}
+	}
+	dst := gocv.NewMat()
+	defer dst.Close()
+
+	edgeWin := gocv.NewWindow("edge")
+	defer edgeWin.Close()
+
+	edgeWin.IMShow(edge)
+
+	gocv.WaitKey(0)
+}
--- common/mat.go
+++ common/mat.go
@@ -1,15 +1,31 @@
 package common
 
-import "gocv.io/x/gocv"
+import (
+	"gocv.io/x/gocv"
+)
 
 func NewMat(row, col int, data [][]float32, dataType gocv.MatType) *gocv.Mat {
-    mat := gocv.Zeros(row, col, dataType)
+	mat := gocv.Zeros(row, col, dataType)
 
-    for i := 0; i < row; i++ {
-        for j := 0; j < col; j++ {
-            mat.SetFloatAt(i, j, data[i][j])
-        }
-    }
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			mat.SetFloatAt(i, j, data[i][j])
+		}
+	}
 
-    return &mat
+	return &mat
+}
+
+func Clip(mat gocv.Mat, min, max float32) gocv.Mat {
+	for i := 0; i < mat.Rows(); i++ {
+		for j := 0; j < mat.Cols(); j++ {
+			if mat.GetFloatAt(i, j) < min {
+				mat.SetFloatAt(i, j, min)
+			}
+			if mat.GetFloatAt(i, j) > max {
+				mat.SetFloatAt(i, j, max)
+			}
+		}
+	}
+	return mat
 }
