f400e615e506884e19167f00ef4a77f313fa2275
kcson <fob330@gmail.com>
2022-05-03 00:06:33 +0900

    commit

--- ch07/namecard.go
+++ ch07/namecard.go
@@ -262,7 +262,7 @@ func (rr *ReleaseResource) SetNext(chain Chain) Chain {
 }
 
 func main() {
-	fileName := "imgs/vehicle16.jpeg"
+	fileName := "imgs/vehicle17.jpeg"
 	if len(os.Args) > 1 {
 		fileName = os.Args[1]
 	}
