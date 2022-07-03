197261288ad9bf550b0bb4711491124f7d76b05e
kcson <fob330@gmail.com>
2022-07-04 02:43:07 +0900

    commit

--- ch08/digitrec.py
+++ ch08/digitrec.py
@@ -4,15 +4,28 @@ import cv2 as cv
 
 
 def load_digits():
-    None
+    img_digits = []
+
+    for i in range(10):
+        filename = '../imgs/digits/digit{}.bmp'.format(i)
+        img_digits.append(cv.imread(filename, cv.IMREAD_GRAYSCALE))
+
+        if img_digits[i] is None:
+            return None
+    return img_digits
 
 
-src = cv.imread("../imgs/digits_print.bmp")
-if src is None:
-    print("image load fail!!")
-    sys.exit()
+def main():
+    src = cv.imread("../imgs/digits_print.bmp")
+    if src is None:
+        print("image load fail!!")
+        sys.exit()
 
-cv.imshow('src', src)
+    cv.imshow('src', src)
 
-cv.waitKey()
-cv.destroyAllWindows()
+    cv.waitKey()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
