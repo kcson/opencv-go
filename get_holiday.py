cd244be5d1a290865f15370c23411576769327d5
angryturtle <tom@last30min.com>
2023-04-28 17:27:44 +0900

    commit

--- get_holiday.py
+++ get_holiday.py
@@ -1,7 +1,18 @@
+import json
+
 import requests
 
+year = '2023'
 url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo'
-params = {'serviceKey': 'kYU0U/7S/1rSI8K9B+0zI6VRUyDuKlImNPSDJAjPlrQiJxeKQ6X25eNH84wDHBX8aEMIfJi9QJ4zTBwQnVWY5w==', 'solYear': '2023','numOfRows': 100}
+params = {'serviceKey': 'kYU0U/7S/1rSI8K9B+0zI6VRUyDuKlImNPSDJAjPlrQiJxeKQ6X25eNH84wDHBX8aEMIfJi9QJ4zTBwQnVWY5w==', 'solYear': year, 'numOfRows': 100, '_type': 'json'}
 
 response = requests.get(url, params=params)
 print(response.text)
+
+jsonObj = json.loads(response.text)
+holiDays = jsonObj['response']['body']['items']['item']
+for holiDay in holiDays:
+    print(holiDay['dateName'])
+    print(holiDay['locdate'])
+    print(holiDay['isHoliday'])
+    print('================================')
