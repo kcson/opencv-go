4d91fbe70a73f2256ed8dfddc4b34c5478e69cfb
kcson <fob330@gmail.com>
2023-06-09 22:52:42 +0900

    commit

--- ch08/eyedetect.py
+++ ch08/eyedetect.py
@@ -8,7 +8,24 @@ if src is None:
     print('image load fail !!')
     sys.exit()
 
-print(src.shape)
+face_classifier = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+eye_classifier = cv.CascadeClassifier('haarcascade_eye.xml')
+if face_classifier.empty() or eye_classifier.empty():
+    print('xml load fail!!')
+    sys.exit()
+
+tm.start()
+faces = face_classifier.detectMultiScale(src, scaleFactor=1.2, minSize=(100, 100))
+for (x1, y1, w1, h1) in faces:
+    cv.rectangle(src, (x1, y1, w1, h1), (255, 0, 255), 2)
+
+    faceROI = src[y1:y1 + h1 // 2, x1:x1 + w1]
+    eyes = eye_classifier.detectMultiScale(faceROI)
+    for (x2, y2, w2, h2) in eyes:
+        center = (x2 + w2 // 2, y2 + h2 // 2)
+        cv.circle(faceROI, center, w2 // 2, (255, 0, 0), 2, cv.LINE_AA)
+tm.stop()
+print(tm.getTimeMilli())
 cv.imshow('src', src)
 cv.waitKey()
 cv.destroyAllWindows()
